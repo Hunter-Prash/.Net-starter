455c6c29dd7ee570651a5932b60ecf69dbf55506
Hunter-Prash <rajaji.prashant@gmail.com>
2026-04-13 19:44:03 +0530

    Add project files.

---
+++ WebApplication1/Controllers/GpuController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GpuController : ControllerBase
+    {
+        private static List<Gpu> gpus = new List<Gpu>
+        {
+            new Gpu { Id = 1, Brand = "NVIDIA", Name = "GeForce RTX 3080", MemorySize = 10, Price = 699 },
+            new Gpu { Id = 2, Brand = "AMD", Name = "Radeon RX 6800 XT", MemorySize = 16, Price = 649 },
+            new Gpu { Id = 3, Brand = "NVIDIA", Name = "GeForce RTX 3070", MemorySize = 8, Price = 499 }
+        };
+
+        [HttpGet]
+
+
+    }
+}
---
+++ WebApplication1/Models/Gpu.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class Gpu
+    {
+        public int Id {get;set; }
+        public string Brand { get; set; } = "";
+        public string Name { get; set; } = "";
+        public int MemorySize { get; set; }
+        public int Price { get; set; }
+
+
+    }
+}
